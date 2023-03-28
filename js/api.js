d2d61b53ced3d9267b4bb5e65d8015cbcf3e72d7
shokha2707 <khamidovserzod@gmail.com>
2023-03-28 14:48:45 +0500

    added

--- js/api.js
+++ js/api.js
@@ -4,28 +4,37 @@ const IMG_URL = `http://www.omdbapi.com/?apikey=${API_KEY}` ;
 
 let elform = document.querySelector("[data-movie-form]");
 let ellist = document.querySelector("[data-movie-list]");
-let eldata = document.querySelector("[data-top]")
+let eldata = document.querySelector("[data-top]");
+var elboxadd = document.querySelector("[data-add-box-movie]");
+var eltemplate = document.querySelector("[data-template-card]");
+//let elForm = document.querySelector("[data-movie-form]");
 
-elform.addEventListener("submit", (evt) =>{
-    evt.preventDefault();
-
-    const formData = new FormData(elform);
-    const name = formData.get("name");
-    searchMovies(name);
-})
+elform.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  const form = new FormData(elform);
+  const name = form.get("search");
+  searchMovies(name);
+});
 
 async function searchMovies(query) {
-    const res = await fetch(`${API_URL}&s=${query}`);
-    const searchResult = await res.json()
-
-    renderMovies(searchResult.Search)
-}
-
-function renderMovies(movies) {
-    ellist.innerHTML = "";
-    let html = ""
-    movies.forEach(movie => {
-       html += `<li><img width="50" src="${movie.Poster}" alt="${movie.Title}"/>${movie.Title}</li>` 
+      let response = await fetch(`${API_KEY}&s=${query}`);
+      let result = await response.json();
+      renderMovie(result);
+  }
+  
+  function renderMovie(arrayFirst) {
+    let array = arrayFirst.Search;
+    elboxadd.innerHTML = "";
+    array.forEach((movie) => {
+        elboxadd.appendChild(createLi(movie));
     });
-    ellist.innerHTML = html
-}
+  }
+  
+  function createLi(movie) {
+    const card = eltemplate.content.cloneNode(true);
+    card.querySelector("[data-movie-poster]").src = movie.Poster;
+    card.querySelector("[data-movie-poster]").alt = movie.Title;
+    card.querySelector("[data-title-movie]").textContent = movie.Title;
+  //  card.querySelector("[data-movie-id]").dataset.id = movie.imdbID;
+    return card;
+  }
